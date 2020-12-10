ffcac1a8a29a9846eace675c10bb18d7500a4663
yosshi4486 <yoshiharuseijin@gmail.com>
2020-12-10 13:08:50 +0900

    doc: Document codes of dfs.

--- Sources/TreeAlgorithm/DFS.swift
+++ Sources/TreeAlgorithm/DFS.swift
@@ -7,6 +7,12 @@
 
 import Foundation
 
+/// Traverses a given tree structured data by Depth First Search.
+///
+/// - Parameters:
+///   - data: The root node of tree structure.
+///   - keypath: The keypath for data's children where the type of Element is equal to the type of Data. The property must be Oprional<Data>.
+///   - forEach: The closure is executed in each node traversal.
 public func dfs<Data>(_ data: Data.Element, children keypath: KeyPath<Data.Element, Data?>, forEach: (Data.Element) -> Void) where Data: RandomAccessCollection {
     forEach(data)
 
@@ -18,6 +24,12 @@ public func dfs<Data>(_ data: Data.Element, children keypath: KeyPath<Data.Eleme
 
 }
 
+/// Traverses a given tree structured data by Depth First Search.
+///
+/// - Parameters:
+///   - data: The root node of tree structure.
+///   - keypath: The keypath for data's children where the type of Element is equal to the type of Data.
+///   - forEach: The closure is executed in each node traversal.
 public func dfs<Data>(_ data: Data.Element, children keypath: KeyPath<Data.Element, Data>, forEach: (Data.Element) -> Void) where Data: RandomAccessCollection {
     forEach(data)
 
