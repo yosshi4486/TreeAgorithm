21d4d47c7f4e4e2c23b8bfffa5234f58c71548b2
yosshi <yoshiharuseijin@gmail.com>
2020-12-10 13:38:16 +0900

    Update DFS.swift

--- Sources/TreeAlgorithm/DFS.swift
+++ Sources/TreeAlgorithm/DFS.swift
@@ -13,6 +13,9 @@ import Foundation
 ///   - data: The root node of tree structure.
 ///   - keypath: The keypath for data's children where the type of Element is equal to the type of Data. The property must be Oprional<Data>.
 ///   - forEach: The closure is executed in each node traversal.
+///
+/// - Complexity:
+/// O(n) where n is number of node in the given tree strucure data.
 public func dfs<Data>(_ data: Data.Element, children keypath: KeyPath<Data.Element, Data?>, forEach: (Data.Element) -> Void) where Data: RandomAccessCollection {
     forEach(data)
 
@@ -30,6 +33,9 @@ public func dfs<Data>(_ data: Data.Element, children keypath: KeyPath<Data.Eleme
 ///   - data: The root node of tree structure.
 ///   - keypath: The keypath for data's children where the type of Element is equal to the type of Data.
 ///   - forEach: The closure is executed in each node traversal.
+///
+/// - Complexity:
+/// O(n) where n is number of node in the given tree strucure data.
 public func dfs<Data>(_ data: Data.Element, children keypath: KeyPath<Data.Element, Data>, forEach: (Data.Element) -> Void) where Data: RandomAccessCollection {
     forEach(data)
 
